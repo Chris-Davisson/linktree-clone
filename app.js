6cc664e61557fd7517e5ef6808848b8e2d00fcfc
Christopher Davisson <davissonchris@gmail.com>
2022-11-24 14:51:51 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const shareButtons = document.querySelectorAll(".tile-share-button");
+async function copyText(e) {
+  e.preventDefault();
+  const link = this.getAttribute("link");
+  try {
+    await navigator.clipboard.writeText(link);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+shareButtons.forEach((shareButton) =>
+  shareButton.addEventListener("click", copyText)
+);
